4fe391762dff70f0896fac73ff01591fb4362bbd
akshayb407 <akshayb407@gmail.com>
2022-07-04 00:17:54 +0530

    StocksManagement added

--- ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -7,21 +7,26 @@ namespace ObjectOrientedPrograms
 {
     internal class Program
     {
+        public static object StocksManagement { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter 0 for InventoryManagement\n");
+            Console.WriteLine("Enter 1 for AddressBook\n" +
+               "Enter 2 for InventoryManagement\n" +
+               "Enter 3 For Stock Management\n"
+               );
             int number = int.Parse(Console.ReadLine());
 
             switch (number)
             {
-                case 0:
-                    InventoryManagement.DiverMethod();
+                case 3:
+                    StockManagement.DriverMethod();
                     break;
                 default:
                     Console.WriteLine("Invalid Entry");
                     break;
-            }
 
+            }
         }
     }
 }
---
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Stock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograms
+{
+    internal class Stock
+    {
+        public String name { get; set; }
+        public int NumberOfShares { get; set; }
+        public int SharePrice { get; set; }
+        public int StockPrice { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0}\nPrice: {1}\nNumber of Shares: {2}",
+                name, SharePrice, NumberOfShares);
+        }
+    }
+
+    public class StockPortfolio : StockPortfolioBase
+    {
+        public int grandTotal { get; set; }
+    }
+}
